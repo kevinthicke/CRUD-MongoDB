2044cffc4ed9c576705efd70770fa6269d055abe
Kevin Cabrera <keviincabrera@gmail.com>
2019-02-26 10:45:26 +0000

    ~

---
+++ server/models/user.js
@@ -0,0 +1,37 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: [ true, 'name is required' ];
+    },
+    email: {
+        type: String,
+        required: [ true, 'email is required']
+    },
+    password: {
+        type: String,
+        required: true
+    }, 
+    img: {
+        type: String,
+        required: false
+    },
+    role: {
+        type: String,
+        default: 'USER_ROLE'
+    },
+    state: {
+        type: Boolean,
+        default: true
+    },
+    google: {
+        type: Boolean,
+        default: false
+    }
+
+})
+
+export default mongoose.model('User', userSchema);
