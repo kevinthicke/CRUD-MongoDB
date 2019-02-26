6f73a6cebae1c6f42116f42bca5e93ad5b39dd23
Kevin Cabrera <keviincabrera@gmail.com>
2019-02-26 10:33:55 +0000

    ~

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
-import express from 'express';
-import colors from 'colors/safe';
 import bodyParser from 'body-parser';
+import colors from 'colors/safe';
+import express from 'express';
+import mongoose from 'mongoose';
 
 import { router as usersRouter } from './routes/users';
 import { PORT } from './config/constants';
@@ -11,6 +12,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use('/users', usersRouter);
+
+mongoose.connect('mongodb://localhost:27017/coffeeDB', (err, response) => {
+    if(err) {
+        throw new Error(colors.red('Connection error with DB'));
+    }
+
+    console.log(colors.bold.green('~ DB connection established successfully'))
+});
 
 app.listen(PORT, () => {
     const msg = colors.bold.cyan(`Server running on localhost:${PORT}`);
